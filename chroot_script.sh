50d1f69e0c7eb1551fd429b888f86147cfca9fd0
wolandark <107309764+wolandark@users.noreply.github.com>
2023-07-28 02:30:54 +0330

    Update chroot_script.sh

--- chroot_script.sh
+++ chroot_script.sh
@@ -2,27 +2,31 @@
 
 ln -sf /usr/share/zoneinfo/Asia/Tehran /etc/localtime
 hwclock --systohc
+
 sed -i 's/^#en_US\.UTF-8/en_US.UTF-8/' /etc/locale.gen
 locale-gen
-pacman -S grub os-prober efibootmgr
+
+pacman -S grub os-prober efibootmgr --noconfirm
 grub-install --recheck /dev/vda
 grub-mkconfig -o /boot/grub/grub.cfg
+
 echo "Enter Root Password"
 passwd
+
 echo "Enter User Name"
 read us
 useradd -m "$us"
 echo "Enter "$us" Password"
 passwd "$us"
 usermod -aG wheel "$us"
+
 echo "artix" > /etc/hostname
 echo "127.0.0.1        localhost
 ::1              localhost
-127.0.1.1        myhostname.localdomain  myhostname
+127.0.1.1        artix.localdomain  artix
 " > /etc/hosts
-pacman -S dhcpcd dhcpcd-runit
-ln -s /etc/runit/sv/dhcp /run/runit/service/
-pacman -S connman-runit connman-gtk
-ln -s /etc/runit/sv/connmand /run/runit/service/
+
+pacman -S connman connman-runit connman-gtk --noconfirm
+ln -s /etc/runit/sv/connmand /etc/runit/runsvdir/default/
 sed -i '/^# %wheel/s/^# //' /etc/sudoers
-pacman -S vim 
+pacman -S vim vi ranger --noconfirm
